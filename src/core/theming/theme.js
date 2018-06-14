412232875c992c5ef8d6f01b579af8ea8f8b6786
antoinechalifour <antoine.chalifour@gmail.com>
2018-06-14 13:43:38 +0200

    feat: Use spotify primary color

--- src/core/theming/theme.js
+++ src/core/theming/theme.js
@@ -2,7 +2,7 @@ export default {
   colors: {
     dark: '#151515',
     danger: '#a63d40',
-    primary: '#fec163',
+    primary: '#1db954',
     confirm: '#90a959',
     accent: '#6494aa',
     typography: '#373d3f',
